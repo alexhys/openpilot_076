6857c81e3272c59a4ca712fa87b51a3113450a05
openpilotkr <64119541+openpilotkr@users.noreply.github.com>
2020-06-25 02:46:18 +0900

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -376,12 +376,14 @@ class CarState(CarStateBase):
       checks += [
         ("E_EMS11", 100),
       ]
+
     if CP.mdpsBus == 0:
       signals += [
         ("CR_Mdps_StrAng", "MDPS11", 0),
         ("CF_Mdps_Stat", "MDPS11", 0),
       ]
-        checks += [("MDPS11", 100)]
+      checks += [("MDPS11", 100)]
+
     return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 0)
 
   @staticmethod
