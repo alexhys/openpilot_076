d86a3dffafa4cab780f141b96ac888b3b99ce54f
multikyd <multikyd@gmail.com>
2020-06-25 14:50:39 +0900

    update

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -142,8 +142,8 @@ class CarState(CarStateBase):
     ret.brakeLights = bool(cp.vl["TCS13"]['BrakeLight'] or ret.brakePressed)
 
     #TODO: find pedal signal for EV/HYBRID Cars
-    ret.gas = cp.vl["EMS12"]['PV_AV_CAN'] / 100
-    ret.gasPressed = bool(cp.vl["EMS16"]["CF_Ems_AclAct"])
+    ret.gas = cp.vl["E_EMS11"]['Accel_Pedal_Pos'] / 100
+    ret.gasPressed = cp.vl["E_EMS11"]['Accel_Pedal_Pos'] > 5
 
     # TODO: refactor gear parsing in function
     # Gear Selection via Cluster - For those Kia/Hyundai which are not fully discovered, we can use the Cluster Indicator for Gear Selection, as this seems to be standard over all cars, but is not the preferred method.
@@ -297,6 +297,8 @@ class CarState(CarStateBase):
 
       ("PV_AV_CAN", "EMS12", 0),
       ("CF_Ems_AclAct", "EMS16", 0),
+      ("Accel_Pedal_Pos","E_EMS11",0),
+      ("Brake_Pedal_Pos","E_EMS11",0),
     ]
 
     checks = [
@@ -312,8 +314,9 @@ class CarState(CarStateBase):
       ("SAS11", 100),
       ("SCC11", 50),
       ("SCC12", 50),
-      #("EMS12", 100),
-      #("EMS16", 100),
+      ("EMS12", 100),
+      ("EMS16", 100),
+      ("E_EMS11", 100),
     ]
     if CP.mdpsBus == 0:
       signals += [
