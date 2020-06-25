65213ff1fe18ae5746c92e5ca0700937f62a13b9
multikyd <multikyd@gmail.com>
2020-06-25 10:12:10 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -278,9 +278,8 @@ class CarInterface(CarInterfaceBase):
     self.cp2.update_strings(can_strings)
     self.cp_cam.update_strings(can_strings)
 
-    ret = self.CS.update(self.cp, self.cp_cam)
-    ret.canValid = self.cp.can_valid and self.cp_cam.can_valid
-
+    ret = self.CS.update(self.cp, self.cp2, self.cp_cam)
+    ret.canValid = self.cp.can_valid and self.cp2.can_valid and self.cp_cam.can_valid
 
     # TODO: button presses
     buttonEvents = []
