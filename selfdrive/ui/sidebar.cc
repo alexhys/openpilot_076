dd65bba09d9cfeb83854768427a88d29b407639e
opbykyd <opbykyd@gmail.com>
2020-06-26 15:26:57 +0900

    Revert "update"
    This reverts commit f887b7f22c22231bc38557d6d04833ffbad7cd58.

--- selfdrive/ui/sidebar.cc
+++ selfdrive/ui/sidebar.cc
@@ -41,20 +41,6 @@ static void ui_draw_sidebar_network_strength(UIState *s) {
   ui_draw_image(s->vg, network_img_x, network_img_y, network_img_w, network_img_h, s->img_network[img_idx], 1.0f);
 }
 
-static void ui_draw_sidebar_ip_addr(UIState *s) {
-  const int network_ip_w = 176;
-  const int network_ip_x = !s->scene.uilayout_sidebarcollapsed ? 58 : -(sbr_w);
-  const int network_ip_y = 255;
-
-  char network_ip_str[15];
-  snprintf(network_ip_str, sizeof(network_ip_str), "%s", s->scene.ipAddr);
-  nvgFillColor(s->vg, COLOR_WHITE);
-  nvgFontSize(s->vg, 32*0.8);
-  nvgFontFace(s->vg, "sans-regular");
-  nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-  nvgTextBox(s->vg, network_ip_x, network_ip_y, network_ip_w, network_ip_str, NULL);
-}
-
 static void ui_draw_sidebar_battery_icon(UIState *s) {
   const int battery_img_h = 36;
   const int battery_img_w = 76;
@@ -71,21 +57,11 @@ static void ui_draw_sidebar_battery_icon(UIState *s) {
                ((battery_img_w - 19) * (batteryPercent * 0.01)), battery_img_h - 11, COLOR_WHITE);
 
   ui_draw_image(s->vg, battery_img_x, battery_img_y, battery_img_w, battery_img_h, battery_img, 1.0f);
-}
 
-static void ui_draw_sidebar_battery_text(UIState *s) {
-  const int battery_img_h = 36;
-  const int battery_img_w = 96;
-  const int battery_img_x = !s->scene.uilayout_sidebarcollapsed ? 150 : -(sbr_w);
-  const int battery_img_y = 303;
+  char temp_value_str1[32];
 
-  char battery_str[7];
-  snprintf(battery_str, sizeof(battery_str), "%d%%%s", s->scene.batteryPercent, strcmp(s->scene.batteryStatus, "Charging") == 0 ? "+" : "-");
-  nvgFillColor(s->vg, COLOR_WHITE);
-  nvgFontSize(s->vg, 36);
-  nvgFontFace(s->vg, "sans-regular");
-  nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-  nvgTextBox(s->vg, battery_img_x, battery_img_y, battery_img_w, battery_str, NULL);
+  snprintf(temp_value_str1, sizeof(temp_value_str1), "%d", batteryPercent );
+  nvgTextBox(s->vg, battery_img_x, battery_img_y - 2, battery_img_w, temp_value_str1, NULL);
 }
 
 static void ui_draw_sidebar_network_type(UIState *s) {
@@ -165,7 +141,7 @@ static void ui_draw_sidebar_temp_metric(UIState *s) {
   const int temp_y_offset = 0;
   snprintf(temp_value_str, sizeof(temp_value_str), "%d", s->scene.paTemp);
   snprintf(temp_value_unit, sizeof(temp_value_unit), "%s", "°C");
-  snprintf(temp_label_str, sizeof(temp_label_str), "%s", "온도");
+  snprintf(temp_label_str, sizeof(temp_label_str), "%s", "TEMP");
   strcat(temp_value_str, temp_value_unit);
 
   ui_draw_sidebar_metric(s, temp_label_str, temp_value_str, temp_severity_map[s->scene.thermalStatus], temp_y_offset, NULL);
@@ -215,8 +191,7 @@ void ui_draw_sidebar(UIState *s) {
   ui_draw_sidebar_settings_button(s);
   ui_draw_sidebar_home_button(s);
   ui_draw_sidebar_network_strength(s);
-  ui_draw_sidebar_ip_addr(s);
-  ui_draw_sidebar_battery_text(s);
+  ui_draw_sidebar_battery_icon(s);
   ui_draw_sidebar_network_type(s);
   ui_draw_sidebar_temp_metric(s);
   ui_draw_sidebar_panda_metric(s);
